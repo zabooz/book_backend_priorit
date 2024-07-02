a165aaf6c5dc72c0d007edb189538a8d20d2ea46
Daniel Osojnik <zabooz1988@gmai.com>
2024-07-02 10:54:05 +0200

    initial commit

---
+++ src/test/java/com/example/book_backend_priorit/BookBackendPrioritApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.book_backend_priorit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookBackendPrioritApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
